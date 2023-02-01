f974425dbfcafd0dbedba3e8b6f49e5c884a6a71
obi-3 <ct4kuto@gmail.com>
2023-02-01 17:59:33 +0900

    Update todobi.go

--- todobi.go
+++ todobi.go
@@ -3,8 +3,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/obi-3/todobi/pkg/tasklist"
 )
 
 func main() {
 	fmt.Println("Welcome to todobi!")
+	t := tasklist.NewTask("hello", 2, 3)
+	fmt.Printf("%v (%v/%v) %v", t.Content(), t.Date().Month(), t.Date().Day(), t.Done())
 }
